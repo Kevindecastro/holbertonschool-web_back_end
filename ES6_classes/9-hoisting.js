41435767d982e92274834555cc860ccadbf8be09
Kevindecastro <9830@holbertonstudents.com>
2025-03-26 14:34:07 +0100

    fix error code

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,5 +1,4 @@
-/* eslint-disable */
-class HolbertonClass {
+export class HolbertonClass {
     constructor(year, location) {
         this._year = year;
         this._location = location;
@@ -13,8 +12,10 @@ class HolbertonClass {
         return this._location;
     }
 }
+const class2019 = new HolbertonClass(2019, 'San Francisco');
+const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-class StudentHolberton {
+export class StudentHolberton {
     constructor(firstName, lastName, holbertonClass) {
         this._firstName = firstName;
         this._lastName = lastName;
@@ -22,7 +23,7 @@ class StudentHolberton {
     }
 
     get fullName() {
-        return `${this._firstName} ${this._lastName}`;
+      return `${this._firstName} ${this._lastName}`;
     }
 
     get holbertonClass() {
@@ -34,17 +35,12 @@ class StudentHolberton {
     }
 }
 
-// Création des objets HolbertonClass
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
-
-// Création des objets StudentHolberton
 const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
 const student2 = new StudentHolberton('John', 'Doe', class2020);
 const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
 const student4 = new StudentHolberton('Donald', 'Bush', class2019);
 const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
 
-// Liste des étudiants
-export const listOfStudents = [student1, student2, student3, student4, student5];
+const listOfStudents = [student1, student2, student3, student4, student5];
+
 export default listOfStudents;
