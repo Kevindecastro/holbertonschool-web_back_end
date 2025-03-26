e633ddae79af6ac741e6d826432895bee567188f
Kevindecastro <9830@holbertonstudents.com>
2025-03-26 14:47:22 +0100

    fix error code

--- ES6_classes/10-car.js
+++ ES6_classes/10-car.js
@@ -1,31 +1,17 @@
 /* eslint-disable */
-const _brand = Symbol('brand');
-const _motor = Symbol('motor');
-const _color = Symbol('color');
-
 export default class Car {
     constructor(brand, motor, color) {
-        this[_brand] = brand;
-        this[_motor] = motor;
-        this[_color] = color;
+        this._brand = brand;
+        this._motor = motor;
+        this._color = color;
+    }
+
+    static get [Symbol.species]() {
+        return this;
     }
 
     cloneCar() {
-        // Créer un nouveau clone avec les mêmes propriétés
-        const clonedCar = new Car(this[_brand], this[_motor], this[_color]);
-        return clonedCar;
-    }
-
-    // Getter pour les propriétés
-    get brand() {
-        return this[_brand];
-    }
-
-    get motor() {
-        return this[_motor];
-    }
-
-    get color() {
-        return this[_color];
+        const Species = this.constructor[Symbol.species];
+        return new Species(this._brand, this._motor, this._color);
     }
 }
