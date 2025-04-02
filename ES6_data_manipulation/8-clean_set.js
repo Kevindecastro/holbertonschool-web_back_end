9e1f18352d5303e4741abbb385fb6b409e343a28
Kevindecastro <9830@holbertonstudents.com>
2025-04-02 12:25:47 +0200

    fix error synthax

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,13 @@
 /* eslint-disable */
 // 8. Fonction extrayant les valeurs d’un Set commençant par une chaîne donnée
 export default function cleanSet(set, startString) {
-    return [...set]
-        .filter(value => value.startsWith(startString))
+    if (!startString || typeof startString !== 'string')
+      return '';
+
+    return Array.from(set)
+        .filter(
+            value => typeof value === 'string' && value.startsWith(startString))
         .map(value => value.slice(startString.length))
         .join('-');
-}
+  }
+ 
