7e5da464c6d3d0a460556935ae1a65003b8bb77e
Kevindecastro <9830@holbertonstudents.com>
2025-04-02 15:40:27 +0200

    fix error

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -5,17 +5,14 @@ import uploadPhoto from './5-photo-reject.js';
 
 // Définition de la fonction handleProfileSignup
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  // On crée les deux promesses signUpUser et uploadPhoto
-  const signUpPromise = signUpUser(firstName, lastName);
-  const uploadPhotoPromise = uploadPhoto(fileName);
-
-  // On utilise Promise.allSettled() pour attendre que les deux promesses soient résolues ou rejetées
-  return Promise.allSettled([signUpPromise, uploadPhotoPromise])
-    .then(results => {
-      // On retourne un tableau d'objets avec les résultats des promesses
-      return results.map(result => ({
-        status: result.status,
-        value: result.status === 'fulfilled' ? result.value : result.reason
-      }));
-    });
+  // Appel des deux fonctions et gestion des promesses avec allSettled
+  return Promise.allSettled([
+	  signUpUser(firstName, lastName),  // Inscription de l'utilisateu
+	  uploadPhoto(fileName),             // Téléchargement de la photo
+  ]).then((results) =>
+	  results.map((result) => ({
+		  status: result.status,
+		  value: result.status === "fulfilled" ? result.value : result.reason,
+	  }))
+  );
 }
