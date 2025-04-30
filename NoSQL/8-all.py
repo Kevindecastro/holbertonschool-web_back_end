81ed2a492b43570227a095f4e9491554cd2cc79a
Kevindecastro <9830@holbertonstudents.com>
2025-04-30 13:59:36 +0200

    fix error

--- NoSQL/8-all.py
+++ NoSQL/8-all.py
@@ -3,4 +3,13 @@
 
 
 def list_all(mongo_collection):
+    """
+    Lists all documents in a collection.
+
+    Args:
+        mongo_collection: The pymongo collection object.
+
+    Returns:
+        A list of all documents, or an empty list if no collection is found.
+    """
     return list(mongo_collection.find())
