96eef521f77ac74473f2e176c34d26eecd338e82
Kevindecastro <9830@holbertonstudents.com>
2025-05-20 11:04:20 +0200

    fix error synthax

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,7 +1,7 @@
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', (name) => {
-  process.stdout.write(`Your name is: ${name}\n`);
+  process.stdout.write(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
